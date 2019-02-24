5c5f6ec308718e15fffe6c49968b13e85c2536c5
Matt <matthewwilshire1@gmail.com>
2019-02-24 16:51:58 +0000

    Activate Winsocket

---
+++ Source/TCP-Server/TCP-Server.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+int main()
+{
+	/* Activate winsocket */
+	WSADATA wsData;
+	WORD version = MAKEWORD(2, 2);
+	int wsDataResult = WSAStartup(version, &wsData);
+	if (wsDataResult != 0)
+	{
+		std::cout << "Windows library failed to initialize." << std::endl;
+		return 0;
+	}
+}
---
+++ Source/TCP-Server/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <ctime>
+
+#include "Ws2tcpip.h"
+
+#define PORT 9977
+
+#endif //PCH_H
