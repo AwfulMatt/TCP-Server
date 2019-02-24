c7d71b7088a989daa60b0c56c96bf3273380e4f2
Matt <matthewwilshire1@gmail.com>
2019-02-24 17:33:17 +0000

    Connection Handling Added

--- Source/TCP-Server/Server.cpp
+++ Source/TCP-Server/Server.cpp
@@ -1,16 +1,14 @@
 #include "pch.h"
 
-#define PORT 9977
+#define PORT 54000
+
+void handleConnection(Connection_t * connection);
+int findConnection(Connection_t * conn);
+
+std::vector<Connection_t *> connections;
 
 Server::Server()
 {
-	/* 
-	Connections stores all connections made as we need a way to 
-	track how many connections there is and allows the server to 
-	send bytes to them. 
-	*/
-	std::vector<Connection_t *> connections;
-
 	/* Creates the socket to listen to the port */
 	SOCKET socketListen = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketListen == INVALID_SOCKET)
@@ -30,7 +28,7 @@ Server::Server()
 	/* Tell the socket to start listening for connections, SOMAXCONN allows max bytes to be sent from the connection! */
 	listen(socketListen, SOMAXCONN);
 
-	std::cout << "Server initialized successfully!" << std::endl;
+	std::cout << std::time(0) << "Server initialized successfully!" << std::endl;
 	std::cout << "Awaiting client connection..." << std::endl;
 
 	/* Client info will store ip and port of connected client! */
@@ -43,5 +41,74 @@ Server::Server()
 	{
 		Connection_t * connection = new Connection_t{ clientSocket, clientInfo };
 		connections.push_back(connection);
+
+		std::thread connectionThread(handleConnection, connection);
+		connectionThread.detach();
+
+		clientSocket = NULL;
 	}
+}
+
+void handleConnection(Connection_t * connection)
+{
+	char host[NI_MAXHOST];
+	char service[NI_MAXHOST];
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXHOST);
+
+	if (getnameinfo((sockaddr*)&connection->addrInfo, sizeof(connection->addrInfo), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		std::cout << host << " connected on port " << service << std::endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &connection->addrInfo.sin_addr, host, NI_MAXHOST);
+		std::cout << host << " connected on port " << ntohs(connection->addrInfo.sin_port) << std::endl;
+	}
+
+	char receiveBuffer[200];
+
+	while (true)
+	{
+		ZeroMemory(receiveBuffer, 200);
+
+		/* Waits here for client to send another message! */
+		int bytesReceived = recv(connection->cSocket, receiveBuffer, 100, 0);
+
+		/* Connection died! */
+		if (bytesReceived == 0 || bytesReceived == SOCKET_ERROR)
+		{
+			std::cout << host << " has disconnected!" << std::endl;
+			connections.erase(connections.begin() + findConnection(connection));
+			break;
+		}
+
+		/* Print to server what the client wrote! */
+		std::cout << host << " : " << receiveBuffer << std::endl;
+
+		/* Tell all clients what the other client wrote! */
+		for (int i = 0; i < connections.size(); i++)
+		{
+			/* Don't send the message to the client it already knows! */
+			if (connections[i] == connection) return;
+			send(connections[i]->cSocket, receiveBuffer, sizeof(receiveBuffer), 0);
+		}
+	}
+}
+
+int findConnection(Connection_t * conn)
+{
+	int found = 0;
+	for (int i = 0; i < connections.size(); i++)
+	{
+		//Two addresses match!
+		if (connections[i] == conn)
+		{
+			found = i;
+			break;
+		}
+	}
+
+	return found;
 }
